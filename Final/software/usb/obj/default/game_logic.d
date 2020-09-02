9562309cb1c8f0ca245e6420596c2aa70e313ba4
ZY-Zong <zhenyu.17@intl.zju.edu.cn>
2020-09-03 02:14:23 +0800

    Kirby support asd keyboard, need test

--- Final/software/usb/obj/default/game_logic.d
+++ Final/software/usb/obj/default/game_logic.d
@@ -1,7 +1,10 @@
-obj/default/game_logic.o: game_logic.c game_logic.h parameter.h kirby.h
+obj/default/game_logic.o: game_logic.c game_logic.h parameter.h kirby.h \
+ test.h
 
 game_logic.h:
 
 parameter.h:
 
 kirby.h:
+
+test.h:
--- Final/software/usb/obj/default/main.d
+++ Final/software/usb/obj/default/main.d
@@ -1,5 +1,6 @@
 obj/default/main.o: main.c ../usb_bsp//HAL/inc/io.h \
- ../usb_bsp//HAL/inc/alt_types.h test.h usb_main.h parameter.h
+ ../usb_bsp//HAL/inc/alt_types.h test.h usb_main.h parameter.h \
+ game_logic.h
 
 ../usb_bsp//HAL/inc/io.h:
 
@@ -10,3 +11,5 @@ test.h:
 usb_main.h:
 
 parameter.h:
+
+game_logic.h:
